4b1b646f4b2207972633d12a0152bf5533a40d63
João Pedro Moraes <joaopedromoraes@outlook.com>
2019-11-19 22:28:52 -0300

    Adiciona criação de grafico com quantidade de licenças encontradas

---
+++ src/statistical-graphs-qtd-licenses.py
@@ -0,0 +1,32 @@
+import statistics as st
+import matplotlib.pyplot as plt
+from numpy import array
+import math
+import pandas as pd
+from statisticalLib import *
+
+with open('./listaDeLicenças.txt') as csvfile:
+        # Adiciona os valores a um objeto, onde cada item vai ser uma linha
+        temp = csv.reader(csvfile, delimiter=',')
+        # Cria lista que vai receber os dados do objeto
+        readCSV = []
+        # Transforma o objeto em uma lista
+        for line in temp:
+                readCSV.append(line)
+        licenses = []
+        for row in readCSV:
+                licenses.append(row[0])
+
+        resumo = set(licenses)
+        quantidade = []
+        for qtd in resumo:
+                quantidade.append(licenses.count(qtd))
+
+        print(quantidade)
+   
+        fig1, ax1 = plt.subplots()
+        ax1.pie(quantidade, labels = resumo, autopct='%1.1f%%', shadow=False, startangle=90)
+        ax1.axis('equal')
+        plt.title('Licenças Com Maior Insidencia')
+        plt.savefig('./graphs/licencas_comuns.png', bbox_inches='tight')
+        plt.show()
