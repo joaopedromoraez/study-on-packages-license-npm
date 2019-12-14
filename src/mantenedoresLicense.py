894bdecdea46c2512037fb2d08a24c696a163f06
João Pedro Moraes <joaopedromoraes@outlook.com>
2019-12-13 22:12:03 -0300

    Adiciona script que retorna o nome do editor de arquivos com licença

---
+++ src/mantenedoresLicense.py
@@ -0,0 +1,39 @@
+# https://github.com/nexB/scancode-toolkit/tree/develop/src/licensedcode/data/licenses -> onde os textos das licenças são encontrados
+import subprocess
+import os
+import csv
+import json
+import pandas as pd
+
+# Função que retorna se um repositorio tem mais de uma licença
+def licencaDuplicada(file):
+    # Abre o arquivo .csv definidor
+    with open(file) as csvfile:
+        # Adiciona os valores a um objeto, onde cada item vai ser uma linha
+        temp = csv.reader(csvfile, delimiter=',')
+        # Cria lista que vai receber os dados do objeto
+        readCSV = []
+        # Transforma o objeto em uma lista
+        for line in temp:
+            readCSV.append(line)
+        #entra no repositorio
+        pathGlobal = readCSV[1][0].replace('/[','').replace(']',' ').replace('/',' ',1).split(' ')
+        os.chdir("repositories/["+pathGlobal[0]+"]"+pathGlobal[1]+"/")
+        if (readCSV != []) and (len(readCSV[0]) > 3):
+            # Vare a lista e buscar as licenças listadas na coluna 'license_expression'e que estejam na raiz do projeto
+            for row in readCSV:
+                path = []
+                #Encontra a ocorrencia de arquivos com licenças
+                if(row[4] != "") and (row[4] != "license__key"):
+                    path = row[0].replace('/[','').replace(']',' ').replace('/',' ',1).split(' ')
+                    # subprocess.getoutput("git shortlog -sen "+path[2]+" >> ~/Desktop/testegitlog.txt")
+                    subprocess.getoutput("git log --pretty=format:'%h;%an;%ae' "+path[2]+" >> ~/Desktop/testegitlog.txt")                    
+
+
+
+inicio = 0
+fim = 2
+
+for sorvete in range(inicio, fim):
+    if (os.path.isfile(f'./summary-licenses-csv/license{str(sorvete)}.csv') == True) :
+        licencaDuplicada(f'./summary-licenses-csv/license{str(sorvete)}.csv')
